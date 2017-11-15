d13bbe5ae097eeacb1b01688953d82ba4cf51252
Christophe Creeten <christophe@holmes.nl>
2017-11-15 10:30:24 +0100

    Initial commit.

---
+++ src/main/kotlin/ExampleDSL.kt
@@ -0,0 +1,89 @@
+import sun.misc.Unsafe
+import java.lang.reflect.Array.newInstance
+import kotlin.reflect.KClass
+import kotlin.reflect.jvm.reflect
+
+val bytes = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 0, 32, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+
+val description = "exampleStruct" with {
+    -{ marker:    String ->    8.bits  }
+    +{ signature: String ->    8.bytes }
+    -{ number:    Int ->       2       } > 0x10
+    -{ size:      Int ->       4       }
+    +{ data:      ByteArray -> 10      }
+}
+
+fun main(args: Array<String>) {
+    val token = description.token
+    println("Struct name: ${token.name}")
+    println("Values: ")
+    token.parse(bytes) forEachValue {
+        val info = description.fieldInformation[it.definition]
+        if (info != null) {
+            val modifier = if (info.first) "public" else "private"
+            val type = info.second.simpleName
+            println("    $modifier $type: $it")
+        }
+    }
+}
+
+infix fun String.with(setup: SequenceBuilder.() -> Unit): MappableToken {
+    val sequenceBuilder = SequenceBuilder(this)
+    sequenceBuilder.setup()
+    return sequenceBuilder.build()
+}
+
+data class MappableToken(val token: Token, val fieldInformation: Map<Def, Pair<Boolean, KClass<*>>>)
+
+val Int.bits: Int
+    get() = this / 8
+
+val Int.bytes: Int
+    get() = this
+
+class SequenceBuilder(private val name: String) {
+
+    val definitions = linkedMapOf<Def, Pair<Boolean, KClass<*>>>()
+
+    inline operator fun <reified T> ((T) -> Int).unaryPlus() {
+        definitions.put(createDefinition(this), Pair(true, T::class))
+    }
+
+    inline operator fun <reified T> ((T) -> Int).unaryMinus() {
+        definitions.put(createDefinition(this), Pair(false, T::class))
+    }
+
+    inline fun <reified T> createDefinition(noinline function: (T) -> Int): Def {
+        val name = function.reflect()?.parameters?.get(0)?.name.orEmpty()
+        val size = function.invoke(createInstanceOfT())
+        return Def(name, size)
+    }
+
+    infix operator fun Unit.compareTo(value: Int) : Int {
+        return 0
+    }
+
+    fun build(): MappableToken {
+        val iterator = definitions.iterator()
+        val sequence = definitions.entries
+                .drop(2)
+                .map { it.key }
+                .fold(Seq(name, iterator.next().key, iterator.next().key), { seq, definition -> Seq(name, seq, definition) })
+        return MappableToken(sequence, definitions)
+    }
+}
+
+inline fun <reified T> createInstanceOfT(): T {
+    return if (isByteArray<T>())
+        newInstance(Byte::class.java, 0) as T
+    else
+        unsafe().allocateInstance(T::class.java) as T
+}
+
+inline fun <reified T> isByteArray(): Boolean = T::class == ByteArray::class
+
+fun unsafe(): Unsafe {
+    val unsafe = Unsafe::class.java.getDeclaredField("theUnsafe")
+    unsafe.isAccessible = true
+    return unsafe.get(null) as Unsafe
+}
---
+++ src/main/kotlin/Tokens.kt
@@ -0,0 +1,37 @@
+typealias ParseResult = Triple<Boolean, Int, List<Value>>
+
+infix fun ParseResult.forEachValue(consumer: (Value) -> Unit) = this.third.forEach(consumer)
+
+data class Value(val definition: Def, val offset: Int) {
+    override fun toString(): String = "Value(name=${definition.name}, size=${definition.size})"
+}
+
+abstract class Token(val name: String) {
+    abstract fun parse(input: ByteArray, offset: Int = 0, result: List<Value> = emptyList()): ParseResult
+}
+
+class Def(name: String, val size: Int) : Token(name) {
+    override fun parse(input: ByteArray, offset: Int, result: List<Value>): ParseResult = if (offset + size <= input.size) Triple(true, offset + size, result + Value(this, offset)) else Triple(false, offset, result)
+}
+
+class Seq(name: String, val left: Token, val right: Token) : Token(name) {
+    constructor(left: Token, right: Token) : this("", left, right)
+
+    override fun parse(input: ByteArray, offset: Int, result: List<Value>): ParseResult = left.parse(input, offset, result).let { if (it.first) right.parse(input, it.second, it.third) else Triple(false, offset, result) }
+}
+
+class Cho(name: String, val left: Token, val right: Token) : Token(name) {
+    constructor(left: Token, right: Token) : this("", left, right)
+
+    override fun parse(input: ByteArray, offset: Int, result: List<Value>): Triple<Boolean, Int, List<Value>> = left.parse(input, offset, result).let { if (it.first) Triple(true, it.second, it.third) else right.parse(input, offset, result) }
+}
+
+class Rep(name: String, val operand: Token) : Token(name) {
+    constructor(operand: Token) : this("", operand)
+
+    tailrec override fun parse(input: ByteArray, offset: Int, result: List<Value>): ParseResult {
+        val (success, outOffset, outResult) = operand.parse(input, offset, result)
+        return if (!success) Triple(true, outOffset, outResult) else parse(input, outOffset, outResult)
+    }
+}
+
